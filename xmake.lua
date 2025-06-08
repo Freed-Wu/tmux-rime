50fe7e9a396a8e81b3021cb300af276e60f7e9d4
Wu, Zhenyu <wuzhenyu@ustc.edu>
2025-06-08 12:47:29 +0800

    :building_construction: Provide a static library

--- xmake.lua
+++ xmake.lua
@@ -7,7 +7,16 @@ add_requires("glib")
 target("tmux-rime")
 do
     set_kind("binary")
-    add_files("*.c")
+    add_files("src/main.c")
+    add_deps("lib")
+end
+
+target("lib")
+do
+    set_kind("static")
+    set_basename("tmux-rime")
+    add_files("src/tmux-rime.c")
+    add_installfiles("src/*.h", { prefixdir = "include" })
     add_packages("rime")
     add_packages("glib")
 end
